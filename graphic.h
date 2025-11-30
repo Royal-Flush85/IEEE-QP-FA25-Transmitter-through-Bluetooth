46433986bd474f99c6a1a8bd77f06f60940aa719
Royal-Flush85 <khasegawa@ucsd.edu>
2025-11-29 23:56:09 -0800

    Add files via upload

---
+++ graphic.h
@@ -0,0 +1,13 @@
+// This file provides placeholder data for bitmaps needed by watch_main.cpp.
+    // Custom fonts (SmallFont, MediumNumbers) are NOT defined here;
+    // the main code now uses Adafruit's built-in setTextSize() to handle sizing.
+
+    // Placeholder for a 16x16 Bluetooth icon bitmap.
+    // This is a simple 16x16 icon placeholder (all white pixels).
+    // The format is for the Adafruit GFX library.
+    const unsigned char bluetooth[] PROGMEM = {
+      0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+      0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+      0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+      0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    };
